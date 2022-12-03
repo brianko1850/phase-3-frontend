c34cca885a763d4876448d4a04c88ae5ad765e5b
brianko <kobj11@gmail.com>
2022-12-02 22:04:03 -0800

    post fucngiotn:

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { useState } from "react";
 import './style.css'
 
 function Add() {
+
+    const [ name, setName] = useState("")
+    const [ weight, setWeight ] = useState(0)
+    const [ doesPull, setDoesPull ] = useState(false)
+    const [ temperment, setTemperment ] = useState("")
+
+  
     return(
-        <h1>New Dogo!</h1>
+        <form onSubmit={(e)=>{
+            e.preventDefault()
+        }}>
+            <h1>Add a Doggo!</h1>
+                <label htmlFor="Name">Name:</label>
+                <input
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}       
+                />
+            <br/>
+            <label htmlFor="Weight">weight:</label>
+            <input
+                type="number"
+                required
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+            />
+            <br/>
+            <label htmlFor="Temperment">Temperment</label>
+            <textarea
+                value={temperment}
+                onChange={(e) => setTemperment(e.target.value)}
+            ></textarea>
+            <label htmlFor="leash puller?">Leash Puller?</label>
+            <input type="checkbox" onClick={(e)=>setDoesPull(e.target.value)}/>
+            <button type="submit">Add</button>
+            <p>
+             {name}
+             <br/>
+             {weight}
+             <br/>
+             {temperment}
+             <br/>
+             {doesPull}
+            </p>
+        </form>
     )
 }
 
